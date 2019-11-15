0587d251528485aeb7130fe2791a4b4e19575135
Chris Marston <marstonc@C02VV35XG8WN.local>
2019-11-15 14:42:42 +0000

    third commit

--- main.tf
+++ main.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol =    "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "example" {
